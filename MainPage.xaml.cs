af6c8ad88edab4c0f63d4ea072f9c3657caff97b
StoFlorin <stoflorin22@gmail.com>
2023-05-31 12:28:45 +0300

    Handle currentWeather requests

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,17 +5,29 @@ namespace WeatherApp.NETMAUI;
 
 public partial class MainPage : ContentPage
 {
+    Coordinates locationCoords = null;
     LocalizationService _localizationService;
+    WeatherService _weatherService;
     public MainPage()
     {
         InitializeComponent();
         _localizationService = new LocalizationService();
+        _weatherService = new WeatherService();
     }
 
     private async void ButtonClicked(object sender, EventArgs e)
     {
-        Coordinates locationCoords = await _localizationService.GetLocation();
-        BindingContext = locationCoords;
+        locationCoords = await _localizationService.GetLocation();
+        CurrentWeather currentWeather = await _weatherService.GetCurrentWeather(GenerateRequest(API.OpenWeatherAPILink));
+        BindingContext = currentWeather;
+    }
+
+    private string GenerateRequest(string endpoint)
+    {
+        string requestUri = endpoint;
+        requestUri += $"weather?lat={locationCoords.Latitude}&lon={locationCoords.Longitude}";
+        requestUri += $"&appid={API.OpenWeatherAPIKey}";
+        return requestUri;
     }
 }
 
